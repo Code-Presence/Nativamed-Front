81cc37a18e51c96a8d5e9f2b95d10ece0c6c1c90
atiladefreitas <atila.defreitas@icloud.com>
2023-02-14 21:23:45 -0300

    responsivity, images and texts

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -25,120 +25,148 @@ import Logo from '../../assets/logo.png';
 import Muie from '../../assets/muie.png';
 import Woman from '../../assets/woman.png';
 import Laptop from '../../assets/laptop.png';
+import Frasco from '../../assets/frasco.png';
 // import {Slider} from '../components/Slider/Slider';
 
 function Home(): JSX.Element {
   return (
     <Container>
-      {/*<Navbar>*/}
-      {/*  <Wrapper*/}
-      {/*    style={{*/}
-      {/*      justifyContent: 'center',*/}
-      {/*      gap: '40px',*/}
-      {/*    }}*/}
-      {/*  >*/}
-      {/*    <img src={Logo} alt="Logo Nativamed"/>*/}
-      {/*    <p>Tenho prescrição</p>*/}
-      {/*    <p>Sobre o tratamento</p>*/}
-      {/*    <p>Perguntas frequentes</p>*/}
-      {/*    <p>Blog</p>*/}
-      {/*    <p>Entrar em contato</p>*/}
-      {/*  </Wrapper>*/}
-      {/*</Navbar>*/}
-      {/*<FirstSection>*/}
-      {/*  <Wrapper>*/}
-      {/*    <LeftWrapper>*/}
-      {/*      <TextWrapper>*/}
-      {/*        <PreTitle>*/}
-      {/*          ALÍVIO NOS SINTOMAS DA ANSIEDADE, INSÔNIA E DORES CRÔNICAS*/}
-      {/*        </PreTitle>*/}
-      {/*        <Title>*/}
-      {/*          Comece hoje o tratamento <br/>*/}
-      {/*          para melhorar sua qualidade <br/>*/}
-      {/*          de vida*/}
-      {/*        </Title>*/}
-      {/*        <SubTitle>NATURAL, LEGAL E APROVADO PELA ANVISA</SubTitle>*/}
-      {/*        <FooterTitle>*/}
-      {/*          Te acolheremos desde o agendamento da sua consulta médica até a*/}
-      {/*          compra do medicamento. <b>Simples e prático!</b>*/}
-      {/*        </FooterTitle>*/}
-      {/*        <Button>Agendar Consulta</Button>*/}
-      {/*      </TextWrapper>*/}
-      {/*    </LeftWrapper>*/}
-      {/*    <RightWrapper>*/}
-      {/*      <img src={Muie} alt={'Mulher'} style={{width: '500px'}}/>*/}
-      {/*    </RightWrapper>*/}
-      {/*  </Wrapper>*/}
-      {/*</FirstSection>*/}
-      {/*<SecondSection>*/}
-      {/*  <SeconSectionDivider>*/}
-      {/*    <Wrapper>*/}
-      {/*      <LeftWrapper>*/}
-      {/*        <img src={Laptop} alt={'Laptop'} style={{width: '400px', marginTop: '220px', position: 'absolute', borderRadius: '8px'}}/>*/}
-      {/*      </LeftWrapper>*/}
-      {/*      <RightWrapper>*/}
-      {/*        <Title>*/}
-      {/*          Preencha o formulário para dar <br/>*/}
-      {/*          o primeiro passo em direção ao <br/>*/}
-      {/*          alívio dos seus sintomas*/}
-      {/*        </Title>*/}
-      {/*        <FooterTitle>*/}
-      {/*          Entraremos em contato para te guiar por todo o caminho.*/}
-      {/*        </FooterTitle>*/}
-      {/*        <Button>Conheça o tratamento</Button>*/}
-      {/*      </RightWrapper>*/}
-      {/*    </Wrapper>*/}
-      {/*  </SeconSectionDivider>*/}
-      {/*</SecondSection>*/}
-      {/*<ThirdSection>*/}
-      {/*  <Wrapper/>*/}
-      {/*</ThirdSection>*/}
-      {/*<FourthSection>*/}
-      {/*  <Wrapper/>*/}
-      {/*</FourthSection>*/}
-      {/*<FifthSection>*/}
-      {/*  <Wrapper></Wrapper>*/}
-      {/*</FifthSection>*/}
-      {/*<SixthSection>*/}
-      {/*  <Wrapper>*/}
-      {/*    <LeftWrapper>*/}
-      {/*      <img src={Woman} alt={'Ôta muié'} style={{width: '500px', borderRadius: '8px'}}/>*/}
-      {/*    </LeftWrapper>*/}
-      {/*    <RightWrapper>*/}
-      {/*      <div*/}
-      {/*        style={{*/}
-      {/*          width: '500px',*/}
-      {/*        }}*/}
-      {/*      >*/}
-      {/*        <Title>Conheça a nossa empresa</Title>*/}
-      {/*        <Text>*/}
-      {/*          Somos a nativamed, uma empresa que surgiu da possibilidade de*/}
-      {/*          melhorar a qualidade de vida das pessoas.*/}
-      {/*          <br/>*/}
-      {/*          <br/>*/}
-      {/*          ‘’Depois que a mãe de uma criança pediu ajuda para importar a*/}
-      {/*          medicação e vimos os benefícios e resultados positivos que esse*/}
-      {/*          tipo de tratamento traz nós soubemos – temos que facilitar o*/}
-      {/*          acesso da população a esse medicamento‘’.*/}
-      {/*          <br/>*/}
-      {/*          <br/>*/}
-      {/*          Hoje já atendemos e ajudamos dezenas de pessoas a amezinar seus*/}
-      {/*          sintomas crônicos e a melhorar sua qualidade de vida.*/}
-      {/*          <br/>*/}
-      {/*          <br/>*/}
-      {/*          Através do nosso ecossistema de profissionais contamos com*/}
-      {/*          diversos médicos especialistas para lhe orientar, consultar e*/}
-      {/*          prescrever os medicamentos de acordo com sua condição e patologia,*/}
-      {/*          facilitando o acesso ao tratamento e seus benefícios.*/}
-      {/*        </Text>*/}
-      {/*      </div>*/}
-      {/*    </RightWrapper>*/}
+      <Navbar>
+        <Wrapper
+          style={{
+            justifyContent: 'center',
+            gap: '40px',
+          }}
+        >
+          <img src={Logo} alt="Logo Nativamed"/>
+          <p>Tenho prescrição</p>
+          <p>Sobre o tratamento</p>
+          <p>Perguntas frequentes</p>
+          <p>Blog</p>
+          <p>Entrar em contato</p>
+        </Wrapper>
+      </Navbar>
+      <FirstSection>
+        <Wrapper>
+          <LeftWrapper>
+            <TextWrapper>
+              <PreTitle>
+                ALÍVIO NOS SINTOMAS DA ANSIEDADE, INSÔNIA E DORES CRÔNICAS
+              </PreTitle>
+              <Title>
+                Comece hoje o tratamento <br/>
+                para melhorar sua qualidade <br/>
+                de vida
+              </Title>
+              <SubTitle>NATURAL, LEGAL E APROVADO PELA ANVISA</SubTitle>
+              <FooterTitle>
+                Te acolheremos desde o agendamento da sua consulta médica até a
+                compra do medicamento. <b>Simples e prático!</b>
+              </FooterTitle>
+              <Button>Agendar Consulta</Button>
+            </TextWrapper>
+          </LeftWrapper>
+          <RightWrapper>
+            <img src={Muie} alt={'Mulher'} className={'img-da-muie'}/>
+          </RightWrapper>
+        </Wrapper>
+      </FirstSection>
+      <SecondSection>
+        <SeconSectionDivider>
+          <div className={'second-sec-wrapper'}>
+            <LeftWrapper className={'second-sec-left-wrapper'}>
+              <img src={Laptop} alt={'Laptop'} className={'laptop'} />
+            </LeftWrapper>
+            <RightWrapper>
+              <div className={'second-sec-right-wrapper'}>
+                <Title>
+                Preencha o formulário para dar <br/>
+                o primeiro passo em direção ao <br/>
+                alívio dos seus sintomas
+                </Title>
+                <br />
+                <FooterTitle>
+                Entraremos em contato para te guiar por todo o caminho.
+                </FooterTitle>
+                <br />
+                <Button>Conheça o tratamento</Button>
+              </div>
+            </RightWrapper>
+          </div>
+        </SeconSectionDivider>
+      </SecondSection>
+      <ThirdSection>
+        <div className={'third-sec-wrapper'}>
+          <div className={'third-sec-left-wrapper'}>
+            <div className={'third-sec-text-wrapper'}>
+              <h4>MÉTODO NATURAL</h4>
+              <h2>O nosso tratamento</h2>
+              <p>
+              O tratamento com óleo de Canabidiol (CBD) medicinal é autorizada pela ANVISA e milhares de pacientes já se beneficiam com o seu uso.
+                <br/>
+                <br/>
+              Existem mais de 20 mil estudos publicados comprovando sua eficácia e segurança para diversas patologias.
+                <br />
+                <br />
+              É um método natural que não causa dependência ou tolerância e que interage com um dos principais sistemas do nosso corpo, trazendo um completo estado de bem-estar físico e mental.
+                <br />
+                <br />
+              Taxa de 79,9% no alívio dos sintomas da ansiedade, comprovada cientificamente.
+              </p>
+              <button>Ver depoimentos</button>
+            </div>
+          </div>
+          <div className={'third-sec-right-wrapper'}>
+            <img src={Frasco} alt={'Frasco'} />
+          </div>
+        </div>
+      </ThirdSection>
+      <FourthSection>
+        <Wrapper/>
+      </FourthSection>
+      <FifthSection>
+        <Wrapper></Wrapper>
+      </FifthSection>
+      <SixthSection>
+        <Wrapper>
+          <LeftWrapper>
+            <img src={Woman} alt={'Ôta muié'} style={{width: '500px', borderRadius: '8px'}}/>
+          </LeftWrapper>
+          <RightWrapper>
+            <div
+              style={{
+                width: '500px',
+              }}
+            >
+              <Title>Conheça a nossa empresa</Title>
+              <Text>
+                Somos a nativamed, uma empresa que surgiu da possibilidade de
+                melhorar a qualidade de vida das pessoas.
+                <br/>
+                <br/>
+                ‘’Depois que a mãe de uma criança pediu ajuda para importar a
+                medicação e vimos os benefícios e resultados positivos que esse
+                tipo de tratamento traz nós soubemos – temos que facilitar o
+                acesso da população a esse medicamento‘’.
+                <br/>
+                <br/>
+                Hoje já atendemos e ajudamos dezenas de pessoas a amezinar seus
+                sintomas crônicos e a melhorar sua qualidade de vida.
+                <br/>
+                <br/>
+                Através do nosso ecossistema de profissionais contamos com
+                diversos médicos especialistas para lhe orientar, consultar e
+                prescrever os medicamentos de acordo com sua condição e patologia,
+                facilitando o acesso ao tratamento e seus benefícios.
+              </Text>
+            </div>
+          </RightWrapper>
 
-      {/*  </Wrapper>*/}
-      {/*</SixthSection>*/}
-      {/*<Footer/>*/}
+        </Wrapper>
+      </SixthSection>
+      <Footer/>
     </Container>
   );
 }
+export default Home;
 
-export {Home};
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -9,6 +9,14 @@ export const Container = styled.section`
   flex-direction: column;
 
   background-color: #eee;
+
+  & .img-da-muie {
+    width: 500px;
+
+    @media screen and (max-width: 500px) {
+      width: 340px;
+    }
+  }
 `;
 
 export const Wrapper = styled.section`
@@ -21,14 +29,11 @@ export const Wrapper = styled.section`
   //border: 1px solid red;
 
   @media screen and (max-width: 500px) {
-    flex-direction: column;
-    & p {
-      display: none;
-    }
+    flex-direction: column-reverse;
   }
 `;
 
-export const Navbar = styled.div`
+export const Navbar = styled.nav`
   width: 100%;
   height: 85px;
   background-color: #34554e;
@@ -47,23 +52,44 @@ export const Navbar = styled.div`
     font-family: "Raleway", sans-serif;
     color: #fff;
   }
+
+  @media screen and (max-width: 500px) {
+    & p {
+      display: none;
+    }
+  }
 `;
 
 export const FirstSection = styled.div`
   width: 100%;
-  height: 740px;
+  height: 720px;
   background-color: #fff;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media screen and (max-width: 500px) {
+    flex-direction: column-reverse;
+    height: fit-content;
+
+    & p {
+    }
+  }
 `;
 
 export const TextWrapper = styled.div`
-  width: 500px;
+  width: 550px;
   height: 450px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  @media screen and (max-width: 500px) {
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    gap: 1rem;
+  }
 `;
 
 export const PreTitle = styled.p`
@@ -72,6 +98,11 @@ export const PreTitle = styled.p`
   font-family: "Raleway", sans-serif;
   color: #345249;
   margin: 0;
+
+  @media screen and (max-width: 500px) {
+    font-size: 16px;
+  }
+
 `;
 
 export const Title = styled.h1`
@@ -80,6 +111,10 @@ export const Title = styled.h1`
   font-size: 36px;
   margin: 0;
   color: #425f4b;
+
+  @media screen and (max-width: 500px) {
+    font-size: 22px;
+  }
 `;
 export const SubTitle = styled.h1`
   font-size: 20px;
@@ -87,6 +122,10 @@ export const SubTitle = styled.h1`
   font-family: "Montserrat", sans-serif;
   color: #345249;
   margin: 0;
+
+  @media screen and (max-width: 500px) {
+  font-size: 14px;
+  }
 `;
 
 export const FooterTitle = styled.p`
@@ -95,6 +134,10 @@ export const FooterTitle = styled.p`
   font-family: "Raleway", sans-serif;
   color: #345249;
   margin: 0;
+
+  @media screen and (max-width: 500px) {
+    font-size: 18px;
+  }
 `;
 
 export const LeftWrapper = styled.div`
@@ -104,6 +147,26 @@ export const LeftWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media screen and (max-width: 500px) {
+    width: 90%;
+    height: 450px;
+    justify-content: flex-start;
+    text-align: center;
+  }
+
+  & .laptop {
+    width: 400px;
+    margin-top: 220px;
+    position: absolute;
+    border-radius: 8px;
+
+    @media screen and (max-width: 500px) {
+      display: none;
+    }
+  }
+
+
 `;
 
 export const Button = styled.button`
@@ -118,6 +181,13 @@ export const Button = styled.button`
   color: #fff;
   margin-top: 30px;
   cursor: pointer;
+
+  @media screen and (max-width: 500px) {
+    width: 100%;
+    height: 3rem;
+    font-size: 18px;
+  }
+
 `;
 export const RightWrapper = styled.div`
   width: 50%;
@@ -126,6 +196,19 @@ export const RightWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media screen and (max-width: 500px) {
+  width: 100%;
+  height: 450px;
+  }
+
+  & .second-sec-right-wrapper {
+    width: 95%;
+    text-align: center;
+    gap: 1rem;
+    height: 60%;
+  }
+
 `;
 
 
@@ -133,6 +216,11 @@ export const SecondSection = styled.div`
   width: 100%;
   height: 600px;
   background-color: white;
+
+  @media screen and (max-width: 500px) {
+  height: 400px;
+
+  }
 `;
 
 export const SeconSectionDivider = styled.div`
@@ -142,6 +230,27 @@ export const SeconSectionDivider = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media screen and (max-width: 500px) {
+    height: 100%;
+  }
+
+  & .second-sec-wrapper {
+    max-width: 1440px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @media screen and (max-width: 500px) {
+
+
+    & .second-sec-left-wrapper{
+      display: none;
+      }
+    }
+  }
 `;
 
 export const ThirdSection = styled.div`
@@ -151,6 +260,82 @@ export const ThirdSection = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  & .third-sec-wrapper {
+    max-width: 1440px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+
+    & .third-sec-left-wrapper {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      & .third-sec-text-wrapper {
+        width: 490px;
+        height: 600px;
+        gap: 1rem;
+        display: flex;
+        flex-direction: column;
+
+        & h4 {
+          font-size: 18px;
+          font-weight: bold;
+          font-family: "Raleway", sans-serif;
+          color: #345249;
+          margin: 0;
+        }
+
+        & h2 {
+          font-size: 32px;
+          margin: 0;
+          font-family: "Montserrat", sans-serif;
+          color: #345249;
+          font-weight: 600;
+        }
+
+        & p {
+          font-size: 20px;
+          font-weight: 400;
+          font-family: "Raleway", sans-serif;
+          color: #345249;
+          margin: 0;
+        }
+
+        & button {
+          height: 50px;
+          width: 385px;
+          border-radius: 8px;
+          border: none;
+          background-color: #33554b;
+          font-family: "Montserrat", sans-serif;
+          font-weight: 700;
+          font-size: 20px;
+          color: #fff;
+          margin-top: 30px;
+          cursor: pointer;
+        }
+      }
+    }
+
+    & .third-sec-right-wrapper {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      & img {
+        width: 450px;
+        border-radius: 8px;
+      }
+    }
+  }
 `;
 
 export const FourthSection = styled.div`
